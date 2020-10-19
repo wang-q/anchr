a19dd2bb261982ba91ba35b37b989f059b60ca4f
Qiang Wang <wang-q@users.noreply.github.com>
2020-10-19 08:38:25 +0800

    Create anchr.rs

---
+++ src/anchr.rs
@@ -0,0 +1,23 @@
+extern crate clap;
+use clap::*;
+
+mod cmd;
+
+fn main() -> std::io::Result<()> {
+    let app = App::new("anchr")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Anchr - Assembler of N-free CHRomosomes")
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(cmd::template::make_subcommand())
+        .subcommand(cmd::dep::make_subcommand());
+
+    // Check which subcomamnd the user ran...
+    match app.get_matches().subcommand() {
+        ("template", Some(sub_matches)) => cmd::template::execute(sub_matches),
+        ("dep", Some(sub_matches)) => cmd::dep::execute(sub_matches),
+        (_, _) => unreachable!(),
+    }?;
+
+    Ok(())
+}
